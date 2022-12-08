c45bc5d560bbd3cf6be77418c82987f28e992a75
SJ <sj.cronje.123@gmail.com>
2022-12-08 14:10:40 -0800

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,10 +6,9 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
-
+int main(void)
 {
-	printf ("with proper grammer, but the outcome is a piece of art,");
+	printf("with proper grammer, but the outcome is a piece of art,");
 	return (0);
 
 }
